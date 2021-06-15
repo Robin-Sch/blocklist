57ec622e5b878500bc17ba3a50c8e6f1e09e6ffe
RobinSchapendonk <ytrobinsch2@gmail.com>
2021-06-15 17:15:11 +0200

    auto remove bs

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ output=./hosts.txt
 
 while read line; do curl $line >> $output; done < $input
 
+sed -i '/^#/d' $output # remove comments
+sed -i '/^0\.0\.0\.0*/!s/.*/0.0.0.0 &/' $output # if not starts with 0.0.0.0, add it
+awk -i inplace '!a[$0]++' $output # remove duplicated
+
 totaldomains=$(awk '!/^#/ && !/^$/{c++}END{print c}' $output | awk '{ len=length($0); res=""; for (i=0;i<=len;i++) { res=substr($0,len-i+1,1) res; if (i > 0 && i < len && i % 3 == 0) { res = "," res } }; print res }')
 echo "$totaldomains domains blocked"
