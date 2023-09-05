3e2cc4edab3354754aad357e249c6d70c0a47ff5
Robin-Sch <robin@robinsch.net>
2023-09-05 22:03:31 +0200

    Updated lists

--- select.sh
+++ select.sh
@@ -10,6 +10,7 @@ enabled=$(whiptail --checklist "Select which topics you want to block" $(stty si
   "general" "" on \
   "advertising" "" on \
   "tracking" "" on \
+  "malicious" "" on \
   "gambling" "" on \
   "porn" "" on \
   "social" "" on \
@@ -69,11 +70,25 @@ case $enabled in
 		echo "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt" >> $output
 		echo "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt" >> $output
 		echo "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt" >> $output
-		echo "https://raw.githubusercontent.com/lightswitch05/hosts/master/docs/lists/tracking-aggressive-extended.txt" >> $output
 		echo "https://blocklistproject.github.io/Lists/tracking.txt" >> $output
 		echo "https://justdomains.github.io/blocklists/lists/easyprivacy-justdomains.txt" >> $output
-		echo "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt" >> $output
-		echo "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt" >> $output
+	;;&
+	*malicious* )
+		echo "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt" >> $output
+		echo "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt" >> $output
+		echo "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt" >> $output
+		echo "https://v.firebog.net/hosts/Prigent-Crypto.txt" >> $output
+		echo "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts" >> $output
+		echo "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt" >> $output
+		echo "https://phishing.army/download/phishing_army_blocklist_extended.txt" >> $output
+		echo "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt" >> $output
+		echo "https://v.firebog.net/hosts/RPiList-Malware.txt" >> $output
+		echo "https://v.firebog.net/hosts/RPiList-Phishing.txt" >> $output
+		echo "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt" >> $output
+		echo "https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts" >> $output
+		echo "https://urlhaus.abuse.ch/downloads/hostfile/" >> $output
+		echo "https://malware-filter.gitlab.io/malware-filter/phishing-filter-hosts.txt" >> $output
+		echo "https://v.firebog.net/hosts/Prigent-Malware.txt" >> $output
 	;;&
 	*gambling* )
 		echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts" >> $output
@@ -86,6 +101,7 @@ case $enabled in
 		echo "https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_top1m.list" >> $output
 		echo "https://blocklistproject.github.io/Lists/porn.txt" >> $output
 		echo "https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/pornography-hosts" >> $output
+		echo "https://v.firebog.net/hosts/Prigent-Adult.txt" >> $output
 	;;&
 	*social* )
 		echo "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/social/hosts" >> $output
@@ -110,6 +126,7 @@ case $enabled in
 	;;&
 	*extreme* )
 		echo "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/ultimate.txt" >> $output
+		echo "https://raw.githubusercontent.com/lightswitch05/hosts/master/docs/lists/tracking-aggressive-extended.txt" >> $output
 	;;&
 esac
 
